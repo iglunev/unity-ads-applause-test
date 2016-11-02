551aece217d9fa8be4b624addf24678470744132
Rasmus Selsmark <rasmus@unity3d.com>
2016-11-02 09:30:52 +0200

    Formatting: Tabs to spaces in C#

--- UnityAdsApplauseTest/Assets/Scripts/Analytics.cs
+++ UnityAdsApplauseTest/Assets/Scripts/Analytics.cs
@@ -7,43 +7,43 @@
 /// </summary>
 public static class Analytics
 {
-	public static void SendInitializeAdsEvent (string gameId)
-	{
-		#if UNITY_ANALYTICS
-		var data = new Dictionary<string, object> ();
-		data.Add ("GameId", gameId);
-		SendCustomEventAndLogError ("AdsInitialized", data);
-		#endif
-	}
+    public static void SendInitializeAdsEvent (string gameId)
+    {
+        #if UNITY_ANALYTICS
+        var data = new Dictionary<string, object> ();
+        data.Add ("GameId", gameId);
+        SendCustomEventAndLogError ("AdsInitialized", data);
+        #endif
+    }
 
-	public static void SendShowAdEvent (string gameId, string placementId)
-	{
-		#if UNITY_ANALYTICS
-		var data = new Dictionary<string, object> ();
-		data.Add ("GameId", gameId);
-		data.Add ("PlacementId", placementId);
-		SendCustomEventAndLogError ("ShowAd", data);
-		#endif
-	}
+    public static void SendShowAdEvent (string gameId, string placementId)
+    {
+        #if UNITY_ANALYTICS
+        var data = new Dictionary<string, object> ();
+        data.Add ("GameId", gameId);
+        data.Add ("PlacementId", placementId);
+        SendCustomEventAndLogError ("ShowAd", data);
+        #endif
+    }
 
-	public static void SendAdFinishedEvent (string gameId, string result)
-	{
-		#if UNITY_ANALYTICS
-		var data = new Dictionary<string, object> ();
-		data.Add ("GameId", gameId);
-		data.Add ("Result", result);
-		SendCustomEventAndLogError ("AdFinished", data);
-		#endif
-	}
+    public static void SendAdFinishedEvent (string gameId, string result)
+    {
+        #if UNITY_ANALYTICS
+        var data = new Dictionary<string, object> ();
+        data.Add ("GameId", gameId);
+        data.Add ("Result", result);
+        SendCustomEventAndLogError ("AdFinished", data);
+        #endif
+    }
 
-	private static void SendCustomEventAndLogError (string customEventName, Dictionary<string, object> data)
-	{
-		#if UNITY_ANALYTICS
-		var result = UnityEngine.Analytics.Analytics.CustomEvent (customEventName, data);
-		if (result != UnityEngine.Analytics.AnalyticsResult.Ok)
-		{
-			UIController.Instance.Log ("Failed to send analytics event: " + result.ToString ());
-		}
-		#endif
-	}
+    private static void SendCustomEventAndLogError (string customEventName, Dictionary<string, object> data)
+    {
+        #if UNITY_ANALYTICS
+        var result = UnityEngine.Analytics.Analytics.CustomEvent (customEventName, data);
+        if (result != UnityEngine.Analytics.AnalyticsResult.Ok)
+        {
+            UIController.Instance.Log ("Failed to send analytics event: " + result.ToString ());
+        }
+        #endif
+    }
 }
